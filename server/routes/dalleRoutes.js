47f2582f11ff6faeb0a1d8e793768bf67defe4fc
Shreya Parkar <shreya.parkar.197@gmail.com>
2023-07-16 17:40:57 +0530

    moving to connect frontend and backend

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,38 @@
+import express, { response } from 'express';
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from 'openai';
+
+dotenv.config();
+// dalle_app : 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
+const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+    res.send('Hello from Dalle');
+})
+
+router.route('/').post(async (req, res) => {
+    try {
+        const { prompt } = req.body;
+        const aiResponse = await openai.createImage({
+            prompt: prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        });
+
+        const image = aiResponse.data.data[0].b64_json;
+
+        res.status(200).json({photo: image })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error ? response.data.error.message);
+    }
+})
+
+export default router;
